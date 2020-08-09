496f0e52a3bf854226ec5b0a619b95903c2c3430
Andrei Gaidai <gaidai4ik@yahoo.com>
2020-08-09 14:50:38 -0400

    step 2 is done

--- src/main/kotlin/com/service/OrdersService.kt
+++ src/main/kotlin/com/service/OrdersService.kt
@@ -1,27 +1,69 @@
 package com.service
 
-import java.util.*
+import kotlin.collections.HashMap
+
+fun main(args: Array<String>) {
+    print("Enter your order: ")
+    val input = readLine()
+    println(input)
+    val orderItems: List<String> = input!!.split(",").map { it -> it.trim() }
+    println(orderItems)
+    val os: OrdersService = OrdersService()
+    val cart = os.createCart(orderItems)
+    val priceHashMap = os.getPrices()
+    var total = os.findTotal(cart, priceHashMap)
+
+    println("total: $$total")
+}
 
 class OrdersService {
-    fun main(args: Array<String>) {
-        print("Enter your order: ")
-        val input = readLine()
-        println(input)
-        val orderItems: List<String> = input!!.split(",").map { it -> it.trim() }
-        println(orderItems)
 
-        val priceHashMap = getPrices()
-        var total = findTotal(orderItems, priceHashMap)
-
-        println("total: $$total")
+    fun createCart(orderItems: List<String>): HashMap<String, Int> {
+        val cart: HashMap<String, Int> = HashMap<String, Int>()
+        for (item in orderItems) {
+            val trimmedItem = item.trim().toLowerCase()
+            var count = cart[trimmedItem]
+            if (count == null) {
+                count = 0
+            }
+            cart[trimmedItem] = count + 1
+        }
+        return cart
     }
 
-    fun findTotal(orderItems: List<String>, priceHashMap: HashMap<String, Double>): Double {
+    fun findTotal(cart: HashMap<String, Int>, priceHashMap: HashMap<String, Double>): Double {
         var total = 0.0
-        for (item in orderItems) {
-            total += priceHashMap.get(item.trim().toLowerCase())!!
+        for ((item, count) in cart) {
+            var adjustedCount = 0
+            if (item.equals("orange")) {
+                // 3 for price of 2 logic, N for price of M
+                val N = 3
+                val M = 2
+                adjustedCount = nForPriceOfMAdjustment(count, N, M)
+            }
+
+            if (item.equals("apple")) {
+                // 3 for price of 2 logic, N for price of M
+                val N = 2
+                val M = 1
+                adjustedCount = nForPriceOfMAdjustment(count, N, M)
+            }
+
+            if (priceHashMap.get(item) != null) {
+                total += priceHashMap.get(item)!! * adjustedCount
+            } else {
+                println("ERROR: unknown item $item")
+            }
         }
         return total
+    }
+
+    fun nForPriceOfMAdjustment(count: Int, N: Int, M: Int): Int {
+        var adjustedCount: Int
+        val quotient = count / N
+        val remainder = count % N
+        adjustedCount = quotient * M + remainder
+        return adjustedCount
     }
 
     fun getPrices(): HashMap<String, Double> {
--- src/test/kotlin/com/service/OrdersServiceTest.kt
+++ src/test/kotlin/com/service/OrdersServiceTest.kt
@@ -25,9 +25,25 @@ class OrdersServiceTest {
     }
 
     @Test
-    fun testFindTotal() {
-        val expectedTotal = 0.85
-        val total = os.findTotal(listOf("apple", "orange"), os.getPrices())
+    fun testCreateCart() {
+        val expectedApples = 1
+        val expectedOranges = 1
+        val cart = os.createCart(listOf("apple", "orange"))
+        assertEquals(cart["apple"], expectedApples)
+        assertEquals(cart["orange"], expectedOranges)
+    }
+
+    @Test
+    fun testNForPriceOfMAdjustment() {
+        val expectedResult = 5
+        val result = os.nForPriceOfMAdjustment(7, 3, 2)
+        assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun testnFindTotal() {
+        val expectedTotal = 1.70
+        val total = os.findTotal(hashMapOf("apple" to 3, "orange" to 3), os.getPrices())
         assertEquals(expectedTotal, total, 0.0)
     }
 }
